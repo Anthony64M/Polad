83a2a1831c049da348ef241d4ff1e29821043a5d
Anthony64M <anthony.mutuku@student.moringaschool.com>
2021-11-18 14:31:50 +0300

    Added the Dashboard and Chart Credentials

---
+++ src/le.js
@@ -0,0 +1,499 @@
+
+function RecentPostsOverview(props) {
+  const {
+    polad,
+    title,
+    title2,
+    title3,
+    title4,
+    name,
+    totalPosts,
+    number,
+    scheduled,
+    number2,
+    deleted,
+    number3,
+    shared,
+    number4,
+    todaysTweets,
+    text1,
+    today,
+    yesterday,
+    number5,
+    number6,
+    number7,
+    number8,
+    number9,
+    number10,
+    number11,
+    number12,
+    number13,
+    number14,
+    number15,
+    number16,
+    number17,
+    number18,
+    number19,
+    number20,
+    number21,
+    number22,
+    number23,
+    number24,
+    number25,
+    number26,
+    number27,
+    number28,
+    number29,
+    number30,
+    number31,
+    number32,
+    number33,
+    number34,
+    number35,
+    fanapproval,
+    number36,
+    shared2,
+    number37,
+    text2,
+    x33M,
+    text3,
+    x3H8M,
+    reactionRate,
+    percent,
+  } = props;
+
+  return (
+    <div className="container-center-horizontal">
+      <div className="recent-posts-overviewscreen">
+        <div className="sidebar-desktop">
+          <div className="poladmulish-bold-gray-chateau-19px">
+            {polad}
+          </div>
+          <div className="overlap-group4">
+            <div className="sidebar-list-inactive">
+              <div className="icon-sidebar-in-tive-1-overview"></div>
+              <div className="titlemulish-normal-gray-chateau-16px">
+                {title}
+              </div>
+            </div>
+            <div className="sidebar-list-active">
+              <div className="selected"></div>
+              <div className="icon-sidebar-active-2-tickets">
+                <img
+                  className="vector"
+                  src="vector-3.svg"
+                />
+              </div>
+              <div className="title-1">
+                {title2}
+              </div>
+            </div>
+          </div>
+          <img
+            className="divider-2"
+            src="divider-1.svg"
+          />
+          <div className="sidebar-list-inactive-1">
+            <div className="icon-sidebar-in-ve-a1-settings"></div>
+            <div className="title-2mulish-normal-gray-chateau-16px">
+              {title3}
+            </div>
+          </div>
+        </div>
+        <div className="overlap-group5">
+          <div className="header-default">
+            <div className="flex-row">
+              <div className="title-3mulish-bold-steel-gray-24px">
+                {title4}
+              </div>
+              <div className="icon-search">
+                <div className="overlap-group2">
+                  <div className="ellipse"></div>
+                  <img
+                    className="vector-1"
+                    src="vector-1.svg"
+                  />
+                </div>
+              </div>
+              <div className="overlap-group1">
+                <img
+                  className="vector-2"
+                  src="vector.svg"
+                />
+                <div className="new"></div>
+              </div>
+              <img
+                className="divider-3"
+                src="divider.svg"
+              />
+              <div className="name">
+                {name}
+              </div>
+              <div className="overlap-group">
+                <img
+                  className="avatar-man-header"
+                  src="avatar-man-header.svg"
+                />
+                <div className="profile-circle"></div>
+              </div>
+            </div>
+            <div className="cards">
+              <div className="overlap-group-1border-1px-snuff">
+                <div className="total-postsmulish-bold-santas-gray-19px">
+                  {totalPosts}
+                </div>
+                <h1 className="numbermulish-bold-steel-gray-40px">
+                  {number}
+                </h1>
+              </div>
+              <div className="overlap-group3">
+                <div className="overlap-group-2">
+                  <div className="sheet"></div>
+                </div>
+                <div className="scheduled">
+                  {scheduled}
+                </div>
+                <div className="number-4">
+                  {number2}
+                </div>
+                <div className="overlap-group1-1">
+                  <img
+                    className="fill-1"
+                    src="fill-1.svg"
+                  />
+                  <img
+                    className="stroke-3"
+                    src="stroke-3.svg"
+                  />
+                  <img
+                    className="stroke-5"
+                    src="stroke-5.svg"
+                  />
+                  <img
+                    className="stroke-7"
+                    src="stroke-7.svg"
+                  />
+                  <img
+                    className="stroke-9"
+                    src="stroke-9.svg"
+                  />
+                </div>
+              </div>
+              <div className="overlap-group1-2border-1px-snuff">
+                <div className="deletedmulish-bold-santas-gray-19px">
+                  {deleted}
+                </div>
+                <div className="numbermulish-bold-steel-gray-40px">
+                  {number3}
+                </div>
+              </div>
+              <div className="overlap-group2-1border-1px-snuff">
+                <div className="sharedmulish-bold-santas-gray-19px">
+                  {shared}
+                </div>
+                <div className="numbermulish-bold-steel-gray-40px">
+                  {number4}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="todays-trendsborder-1px-snuff">
+            <div className="flex-col">
+              <div className="flex-row-1">
+                <div className="flex-col-1">
+                  <div className="todays-tweets">
+                    {todaysTweets}
+                  </div>
+                  <p className="text-1">
+                    {text1}
+                  </p>
+                </div>
+                <div className="graph-help">
+                  <div className="today">
+                    <img
+                      className="divider"
+                      src="divider-2.svg"
+                    />
+                    <div className="today-1mulish-semi-bold-santas-gray-12px">
+                      {today}
+                    </div>
+                  </div>
+                  <div className="yesterday">
+                    <img
+                      className="divider"
+                      src="divider-3.svg"
+                    />
+                    <div className="yesterday-1mulish-semi-bold-santas-gray-12px">
+                      {yesterday}
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="full-graph">
+                <div className="overlap-group-3">
+                  <div className="horizontal-lines">
+                    <img
+                      className="divider-4"
+                      src="divider-4.svg"
+                    />
+                    <img
+                      className="divider-1"
+                      src="divider-4.svg"
+                    />
+                    <img
+                      className="divider-1"
+                      src="divider-4.svg"
+                    />
+                    <img
+                      className="divider-1"
+                      src="divider-4.svg"
+                    />
+                    <img
+                      className="divider-1"
+                      src="divider-4.svg"
+                    />
+                    <img
+                      className="divider-1"
+                      src="divider-4.svg"
+                    />
+                    <img
+                      className="divider-1"
+                      src="divider-4.svg"
+                    />
+                  </div>
+                  <div className="divider-graph-vertical">
+                    <img
+                      className="divider-5"
+                      src="divider-11.svg"
+                    />
+                  </div>
+                  <div className="valuesmulish-normal-santas-gray-10px">
+                    <div className="number-5">
+                      {number5}
+                    </div>
+                    <div className="number-1">
+                      {number6}
+                    </div>
+                    <div className="number-1">
+                      {number7}
+                    </div>
+                    <div className="number-1">
+                      {number8}
+                    </div>
+                    <div className="number-1">
+                      {number9}
+                    </div>
+                    <div className="number-1">
+                      {number10}
+                    </div>
+                    <div className="number-6">
+                      {number11}
+                    </div>
+                  </div>
+                  <img
+                    className="graphicmain"
+                    src="graphic-main.svg"
+                  />
+                </div>
+                <div className="hoursmulish-normal-santas-gray-10px">
+                  <div className="number-7">
+                    {number12}
+                  </div>
+                  <div className="number-2">
+                    {number13}
+                  </div>
+                  <div className="number-2">
+                    {number14}
+                  </div>
+                  <div className="number-2">
+                    {number15}
+                  </div>
+                  <div className="number-2">
+                    {number16}
+                  </div>
+                  <div className="number-2">
+                    {number17}
+                  </div>
+                  <div className="number-2">
+                    {number18}
+                  </div>
+                  <div className="number-2">
+                    {number19}
+                  </div>
+                  <div className="number-2">
+                    {number20}
+                  </div>
+                  <div className="number-2">
+                    {number21}
+                  </div>
+                  <div className="number-3">
+                    {number22}
+                  </div>
+                  <div className="number-3">
+                    {number23}
+                  </div>
+                  <div className="number-3">
+                    {number24}
+                  </div>
+                  <div className="number-3">
+                    {number25}
+                  </div>
+                  <div className="number-3">
+                    {number26}
+                  </div>
+                  <div className="number-3">
+                    {number27}
+                  </div>
+                  <div className="number-3">
+                    {number28}
+                  </div>
+                  <div className="number-3">
+                    {number29}
+                  </div>
+                  <div className="number-3">
+                    {number30}
+                  </div>
+                  <div className="number-3">
+                    {number31}
+                  </div>
+                  <div className="number-3">
+                    {number32}
+                  </div>
+                  <div className="number-3">
+                    {number33}
+                  </div>
+                  <div className="number-3">
+                    {number34}
+                  </div>
+                  <div className="number-3">
+                    {number35}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="additional-info">
+              <div className="overlap-group2-2">
+                <div className="fan-approvalmulish-semi-bold-santas-gray-16px">
+                  {fanapproval}
+                </div>
+                <div className="number-8mulish-bold-steel-gray-24px">
+                  {number36}
+                </div>
+                <div className="overlap-group1-3">
+                  <img
+                    className="divider-6"
+                    src="divider-12.svg"
+                  />
+                  <img
+                    className="divider-7"
+                    src="divider-13.svg"
+                  />
+                  <img
+                    className="divider-8"
+                    src="divider-13.svg"
+                  />
+                  <img
+                    className="divider-9"
+                    src="divider-13.svg"
+                  />
+                  <img
+                    className="divider-10"
+                    src="divider-13.svg"
+                  />
+                </div>
+                <div className="shared-1mulish-semi-bold-santas-gray-16px">
+                  {shared2}
+                </div>
+                <div className="number-9mulish-bold-steel-gray-24px">
+                  {number37}
+                </div>
+                <div className="text-2mulish-semi-bold-santas-gray-16px">
+                  {text2}
+                </div>
+                <div className="x33mmulish-bold-steel-gray-24px">
+                  {x33M}
+                </div>
+                <div className="text-3mulish-semi-bold-santas-gray-16px">
+                  {text3}
+                </div>
+                <div className="x3h-8mmulish-bold-steel-gray-24px">
+                  {x3H8M}
+                </div>
+                <div className="reaction-ratemulish-semi-bold-santas-gray-16px">
+                  {reactionRate}
+                </div>
+                <div className="percentmulish-bold-steel-gray-24px">
+                  {percent}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const recentPostsOverviewData = {
+    polad: "POLAD",
+    title: "Scheduler",
+    title2: "Recent  Posts",
+    title3: "Settings",
+    title4: "Overview",
+    name: "Jymal Anthony",
+    totalPosts: "Total Posts",
+    number: "120",
+    scheduled: "Scheduled",
+    number2: "16",
+    deleted: "Deleted",
+    number3: "43",
+    shared: "Shared",
+    number4: "64",
+    todaysTweets: "Today’s tweets",
+    text1: "as of 25 May 2019, 09:41 PM",
+    today: "Today",
+    yesterday: "Yesterday",
+    number5: "60",
+    number6: "50",
+    number7: "40",
+    number8: "30",
+    number9: "20",
+    number10: "10",
+    number11: "0",
+    number12: "0",
+    number13: "1",
+    number14: "2",
+    number15: "3",
+    number16: "4",
+    number17: "5",
+    number18: "6",
+    number19: "7",
+    number20: "8",
+    number21: "9",
+    number22: "10",
+    number23: "11",
+    number24: "12",
+    number25: "13",
+    number26: "14",
+    number27: "15",
+    number28: "16",
+    number29: "17",
+    number30: "18",
+    number31: "19",
+    number32: "20",
+    number33: "21",
+    number34: "22",
+    number35: "23",
+    fanapproval: "FanApproval",
+    number36: "449",
+    shared2: "Shared",
+    number37: "426",
+    text2: "Average first response time",
+    x33M: "33m",
+    text3: "Average response time",
+    x3H8M: "3h 8m",
+    reactionRate: "Reaction rate",
+    percent: "94%",
+};
+
+         
