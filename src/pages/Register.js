8711c2f5d538f4f19ac291e4cdc046fd71ff15ff
Anthony64M <anthony.mutuku@student.moringaschool.com>
2021-11-24 08:53:33 +0300

    Edited the javascript for registry

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,43 +1,66 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Register.css';
 
-function Register() {
-    
+
+class Register extends React.Component {
+  state = {
+    username: '',
+    password: ''
+  };
+
+  handle_change = e => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState(prevstate => {
+      const newState = { ...prevstate };
+      newState[name] = value;
+      return newState;
+    });
+  };
+
+  render() {
     return (
+        <form onSubmit={e => this.props.handle_signup(e, this.state)}>
         <div className="container-center-horizontal mt-5">
             <div className="cards-default">
                 <form>
                     <h3>Sign up</h3>
                     
                     <div className="form-group mt-2">
-                        <label>First Name</label>
-                        <input type="text" className="form-control" placeholder="First Name" />
+                        <label>User Name</label>
+                        <input type="text" className="form-control" placeholder="User Name" 
+                         value={this.state.username}
+                             onChange={this.handle_change}
+                              />
                     </div>
                     
-                    <div className="form-group mt-2">
-                        <label>Last Name</label>
-                        <input type="text" className="form-control" placeholder="Last Name" />
-                    </div>
-                    
-                    <div className="form-group mt-2">
-                        <label>Email</label>
-                        <input type="email" className="form-control" placeholder="Email" />
-                    </div>
 
                     <div className="form-group mt-2">
                         <label>Password</label>
-                        <input type="password" className="form-control" placeholder="Password" />
+                        <input type="password" className="form-control" placeholder="Password"
+                          value={this.state.password}
+                             onChange={this.handle_change}
+                            />
                     </div>
 
                     <div className="form-group mt-3">
                         <label>Confirm Password</label>
                         <input type="password" className="form-control" placeholder="Confirm Password" />
                     </div>
-                    <button className="btn btn-primary btn-block mt-3">Sign up</button>                
+                    <button className="btn btn-primary btn-block mt-3">Register</button>                
                 </form>
             </div>
         </div>
+    </form>
     );
+  }
 }
 
-export default Register
+    export default Register;
+
+    Register.propTypes = {
+      Register: PropTypes.func.isRequired
+    };
+    
+
