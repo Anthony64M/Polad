3e9bb61107dc3dbf38d556ee615c6f7b7976b2ee
Anthony64M <anthony.mutuku@student.moringaschool.com>
2021-11-24 08:54:41 +0300

    Edited the javascript for App

--- src/App.js
+++ src/App.js
@@ -2,26 +2,30 @@ import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from './pages/Dashboard';
-import Register from './pages/Register';
+import Register from './views/auth/Register';
 import './App.css';
 import Landing from './components/Landing';
-import Login from './pages/Login';
+import Login from './views/auth/Login';
+import Logout from './views/auth/Logout';
 import './index.css';
 import Navbar from './components/Navbar';
+import Schedule from './pages/schedule/Schedule';
 
-function App() {
-  return (
-    <>
+const App = () => {
+    return (
+    <div className='App'>
       <Router>
           <Routes>
             <Route exact path="/" element={<Landing />} />
             <Route exact path="dashboard" element={<Dashboard />} />
             <Route exact path="register" element={<Register />} />
             <Route exact path="login" element={<Login />} />
+            <Route exact path="logout" element={<Logout />} />
+            <Route exact path="scheduler" element={<Schedule />} />
           </Routes>
           <Navbar />
       </Router>
-    </>
+    </div>
   );
 }
 
